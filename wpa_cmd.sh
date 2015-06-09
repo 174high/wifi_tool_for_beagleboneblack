0723d55217bb80c01e6591c6db2c2f3f9eeec624
root <root@ubuntu.(none)>
2015-06-09 10:46:07 +0800

    updata conf

--- wpa_cmd.sh
+++ wpa_cmd.sh
@@ -1,1 +1,5 @@
-wpa_supplicant -d -Dnl80211 -c/etc/wpa_supplicant.conf -iwlan0 -B
+wpa_supplicant -d -Dnl80211 -c/data/misc/wifi/wpa_supplicant.conf -iwlan0 -B
+hostapd -d  /data/misc/wifi/hostapd.conf
+
+wpa_supplicant  -iwlan0 -Dnl80211 -c/data/misc/wifi/wpa_supplicant.conf -N -ip2p0 -Dnl80211 -c/data/misc/wifi/p2p_supplicant.conf &
+
