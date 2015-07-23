da0d74f9345a04ec861635cea0dd81d804c1bc9a
root <root@ubuntu.(none)>
2015-07-23 17:11:59 +0800

    newest configure everything is ok

--- push_conf.sh
+++ push_conf.sh
@@ -2,5 +2,6 @@ adb push p2p_supplicant.conf  /data/misc/wifi
 adb push wpa_supplicant.conf  /data/misc/wifi
 adb push udhcpd.conf         /data/misc/wifi
 adb push hostapd.conf        /data/misc/wifi
-adb push hostapd             /system/bin/
-adb push  busybox            /system/bin/
+adb push hostapd.conf        /system/etc/wifi
+adb push hostapd             /system/bin
+adb push  busybox            /system/bin
